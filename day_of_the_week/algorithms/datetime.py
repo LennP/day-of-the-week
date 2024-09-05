07f297b028e6df7eb9e683c4b4d95ec917165687
Lenn <lennperik@hotmail.nl>
2024-09-05 22:07:19 +0200

    Fix datetime

--- day_of_the_week/algorithms/datetime.py
+++ day_of_the_week/algorithms/datetime.py
@@ -8,7 +8,7 @@ from day_of_the_week.models import Weekday
 def date_to_day_of_week(date: datetime) -> Weekday:
     """Returns the day of the week for a given date."""
 
-    return Weekday(date.isoweekday())
+    return Weekday(date.isoweekday() % 7)
 
 
 if __name__ == "__main__":
